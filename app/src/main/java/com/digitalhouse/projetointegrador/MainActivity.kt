0825445ba0635270e979e3dd9ba66e7c6b994fa0
Bruno <brunorbds50@gmail.com>
2020-11-21 16:18:07 -0300

    RecyclerView ViewMore add

--- app/src/main/java/com/digitalhouse/projetointegrador/MainActivity.kt
+++ app/src/main/java/com/digitalhouse/projetointegrador/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment = ViewMoreFragment.newInstance()
+
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.fl_main_test, fragment)
+            addToBackStack(null)
+            commit()
+        }
     }
 }
---
+++ app/src/main/java/com/digitalhouse/projetointegrador/domain/ViewMoreCard.kt
@@ -0,0 +1,7 @@
+package com.digitalhouse.projetointegrador.domain
+
+data class ViewMoreCard(
+    val id: Int,
+    val title: String,
+    val cardImg: Int
+)
