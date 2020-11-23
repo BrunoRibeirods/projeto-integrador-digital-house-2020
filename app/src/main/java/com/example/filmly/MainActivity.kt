626b38df63e093bea420dacb1e937a1867b8aa36
Bruno Saunders <senabruno97@hotmail.com>
2020-11-23 14:27:07 -0300

    integrating bottom navigation bar with navController

--- app/src/main/java/com/example/filmly/MainActivity.kt
+++ app/src/main/java/com/example/filmly/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.filmly
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,26 +11,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bottom_navigation.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.page_home -> {
-                    findNavController(R.id.navHostFragment_MainActivity).navigate(R.id.homeFragment)
-                    true
-                }
-                R.id.page_search -> {
-                    findNavController(R.id.navHostFragment_MainActivity).navigate(R.id.searchFragment)
-                    true
-                }
-                R.id.page_lists -> {
-                    findNavController(R.id.navHostFragment_MainActivity).navigate(R.id.yourListsFragment)
-                    true
-                }
-                R.id.page_profile -> {
-                    findNavController(R.id.navHostFragment_MainActivity).navigate(R.id.profileFragment)
-                    true
-                }
-                else -> false
-            }
-        }
+        setNavigationController()
+    }
+
+    fun setNavigationController() {
+        val navController = findNavController(R.id.navHostFragment_MainActivity)
+        bottom_navigation.setupWithNavController(navController)
     }
 }
