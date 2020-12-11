305f3a87c92e1fdf1594504cb364f936e504e13f
Bruno Saunders <senabruno97@hotmail.com>
2020-12-11 15:33:13 -0300

    cardDetail conflict resolved

--- app/src/main/java/com/example/filmly/ui/cardDetail/CardDetailFragment.kt
+++ app/src/main/java/com/example/filmly/ui/cardDetail/CardDetailFragment.kt
@@ -1,5 +1,8 @@
 package com.example.filmly.ui.cardDetail
 
+import android.graphics.drawable.LayerDrawable
+import android.graphics.text.LineBreaker.JUSTIFICATION_MODE_INTER_WORD
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -16,6 +19,7 @@ import com.example.filmly.data.model.CardDetail
 import com.example.filmly.data.model.Film
 import com.example.filmly.data.model.Serie
 import com.example.filmly.repository.ServicesRepository
+import com.google.android.material.appbar.AppBarLayout
 import kotlinx.android.synthetic.main.fragment_card_detail.view.*
 
 class CardDetailFragment : Fragment() {
@@ -36,6 +40,14 @@ class CardDetailFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_card_detail, container, false)
 
         repository = ServicesRepository.getInstance(requireContext())
+
+        view.app_bar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val offsetAlpha: Float = appBarLayout.y / view.app_bar.getTotalScrollRange()
+            view.iv_cardDetailImageBlur.setAlpha(offsetAlpha * -1)
+        })
+
+        (view.toolbar.navigationIcon as LayerDrawable).findDrawableByLayerId(R.id.seta_cardDetail).setTint(view.resources.getColor(R.color.white))
+        view.toolbar.setNavigationOnClickListener { activity?.onBackPressed() }
 
         val circularProgressDrawable = CircularProgressDrawable(view.context)
         circularProgressDrawable.strokeWidth = 5f
@@ -62,6 +74,10 @@ class CardDetailFragment : Fragment() {
                 CardDetail.SERIE -> viewModel.insertSerie(detail.card as Serie)
                 CardDetail.ACTOR -> viewModel.insertActor(detail.card as Actor)
             }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            view.tv_sinopseCardDetail.justificationMode = JUSTIFICATION_MODE_INTER_WORD
         }
 
         return view
