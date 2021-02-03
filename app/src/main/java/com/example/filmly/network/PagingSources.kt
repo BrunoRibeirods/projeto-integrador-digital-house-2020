986c66b58b9c6bd6a63a07951697f0f66c742b1f
Bruno Saunders <senabruno97@hotmail.com>
2021-02-02 22:10:51 -0300

    all pagingSources

--- app/src/main/java/com/example/filmly/network/PagingSources.kt
+++ app/src/main/java/com/example/filmly/network/PagingSources.kt
@@ -1,18 +1,20 @@
 package com.example.filmly.network
 
 import androidx.paging.PagingSource
+import com.example.filmly.ui.home.PopularActor
 import com.example.filmly.ui.home.PopularMovie
+import com.example.filmly.ui.home.PopularTV
 import retrofit2.HttpException
 import java.io.IOException
 
-private const val MOVIES_STARTING_PAGE_INDEX = 1
+private const val STARTING_PAGE_INDEX = 1
 
 class MoviesPagingSource(
     private val service: TmdbApi
 ) : PagingSource<Int, PopularMovie>() {
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, PopularMovie> {
-        val position = params.key ?: MOVIES_STARTING_PAGE_INDEX
+        val position = params.key ?: STARTING_PAGE_INDEX
         return try {
             val response = service.getAllPopularMovies(position)
             val movies = response.results
@@ -23,7 +25,61 @@ class MoviesPagingSource(
             }
             LoadResult.Page(
                 data = movies,
-                prevKey = if (position == MOVIES_STARTING_PAGE_INDEX) null else position - 1,
+                prevKey = if (position == STARTING_PAGE_INDEX) null else position - 1,
+                nextKey = nextKey
+            )
+        } catch (exception: IOException) {
+            return LoadResult.Error(exception)
+        } catch (exception: HttpException) {
+            return LoadResult.Error(exception)
+        }
+    }
+}
+
+class TVPagingSource(
+    private val service: TmdbApi
+) : PagingSource<Int, PopularTV>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, PopularTV> {
+        val position = params.key ?: STARTING_PAGE_INDEX
+        return try {
+            val response = service.getAllPopularTV(position)
+            val series = response.results
+            val nextKey = if (series.isEmpty()) {
+                null
+            } else {
+                position + 1
+            }
+            LoadResult.Page(
+                data = series,
+                prevKey = if (position == STARTING_PAGE_INDEX) null else position - 1,
+                nextKey = nextKey
+            )
+        } catch (exception: IOException) {
+            return LoadResult.Error(exception)
+        } catch (exception: HttpException) {
+            return LoadResult.Error(exception)
+        }
+    }
+}
+
+class ActorsPagingSource(
+    private val service: TmdbApi
+) : PagingSource<Int, PopularActor>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, PopularActor> {
+        val position = params.key ?: STARTING_PAGE_INDEX
+        return try {
+            val response = service.getAllPopularActors(position)
+            val actors = response.results
+            val nextKey = if (actors.isEmpty()) {
+                null
+            } else {
+                position + 1
+            }
+            LoadResult.Page(
+                data = actors,
+                prevKey = if (position == STARTING_PAGE_INDEX) null else position - 1,
                 nextKey = nextKey
             )
         } catch (exception: IOException) {
