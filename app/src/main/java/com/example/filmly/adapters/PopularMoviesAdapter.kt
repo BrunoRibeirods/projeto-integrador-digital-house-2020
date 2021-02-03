0ba3b4537f90e94f9e1c8b139b23374416db8df3
Bruno Saunders <senabruno97@hotmail.com>
2021-02-02 22:24:47 -0300

    new adapters of HomeFragment

--- app/src/main/java/com/example/filmly/adapters/PopularMoviesAdapter.kt
+++ app/src/main/java/com/example/filmly/adapters/PopularMoviesAdapter.kt
@@ -73,3 +73,123 @@ class PopularMoviesAdapter(
         }
     }
 }
+
+class PopularTVAdapter(
+    val cardInfo: Int,
+    val cardNavigation: CardDetailNavigation,
+) : PagingDataAdapter<PopularMovie, PopularTVAdapter.PopularTVViewHolder>(
+    POPULAR_TV_COMPARATOR) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularTVViewHolder {
+        return PopularTVViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: PopularTVViewHolder, position: Int) {
+        val item = getItem(position)
+        if (item != null) {
+            holder.bind(item, cardInfo, cardNavigation)
+        }
+    }
+
+    companion object {
+        private val POPULAR_TV_COMPARATOR = object : DiffUtil.ItemCallback<PopularMovie>() {
+            override fun areItemsTheSame(oldItem: PopularMovie, newItem: PopularMovie): Boolean =
+                oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: PopularMovie, newItem: PopularMovie): Boolean =
+                oldItem == newItem
+        }
+    }
+
+    class PopularTVViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(item: PopularMovie, cardInfo: Int, cardNavigation: CardDetailNavigation) {
+
+            val circularProgressDrawable = CircularProgressDrawable(view.context)
+            circularProgressDrawable.strokeWidth = 5f
+            circularProgressDrawable.setColorSchemeColors(
+                view.resources.getColor(R.color.color_main)
+            )
+            circularProgressDrawable.centerRadius = 30f
+            circularProgressDrawable.start()
+
+            Glide.with(view).asBitmap()
+                .load("https://image.tmdb.org/t/p/w500${item.poster_path}")
+                .placeholder(circularProgressDrawable)
+                .error(R.drawable.placeholder)
+                .fallback(R.drawable.placeholder)
+                .into(view.iv_cardImage)
+
+            val cardDetail = CardDetail(cardInfo, item.convertToFilm())
+            view.iv_cardImage.setOnClickListener {
+                cardNavigation.onClick(cardDetail)
+            }
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): PopularTVViewHolder {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.cards_list_item, parent, false)
+                return PopularTVViewHolder(view)
+            }
+        }
+    }
+}
+
+class PopularActorsAdapter(
+    val cardInfo: Int,
+    val cardNavigation: CardDetailNavigation,
+) : PagingDataAdapter<PopularMovie, PopularActorsAdapter.PopularActorsViewHolder>(
+    POPULAR_ACTOR_COMPARATOR) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PopularActorsViewHolder {
+        return PopularActorsViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: PopularActorsViewHolder, position: Int) {
+        val item = getItem(position)
+        if (item != null) {
+            holder.bind(item, cardInfo, cardNavigation)
+        }
+    }
+
+    companion object {
+        private val POPULAR_ACTOR_COMPARATOR = object : DiffUtil.ItemCallback<PopularMovie>() {
+            override fun areItemsTheSame(oldItem: PopularMovie, newItem: PopularMovie): Boolean =
+                oldItem.id == newItem.id
+
+            override fun areContentsTheSame(oldItem: PopularMovie, newItem: PopularMovie): Boolean =
+                oldItem == newItem
+        }
+    }
+
+    class PopularActorsViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(item: PopularMovie, cardInfo: Int, cardNavigation: CardDetailNavigation) {
+
+            val circularProgressDrawable = CircularProgressDrawable(view.context)
+            circularProgressDrawable.strokeWidth = 5f
+            circularProgressDrawable.setColorSchemeColors(
+                view.resources.getColor(R.color.color_main)
+            )
+            circularProgressDrawable.centerRadius = 30f
+            circularProgressDrawable.start()
+
+            Glide.with(view).asBitmap()
+                .load("https://image.tmdb.org/t/p/w500${item.poster_path}")
+                .placeholder(circularProgressDrawable)
+                .error(R.drawable.placeholder)
+                .fallback(R.drawable.placeholder)
+                .into(view.iv_cardImage)
+
+            val cardDetail = CardDetail(cardInfo, item.convertToFilm())
+            view.iv_cardImage.setOnClickListener {
+                cardNavigation.onClick(cardDetail)
+            }
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): PopularActorsViewHolder {
+                val view = LayoutInflater.from(parent.context).inflate(R.layout.cards_list_item, parent, false)
+                return PopularActorsViewHolder(view)
+            }
+        }
+    }
+}
